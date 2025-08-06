22a7b794228c69ad10a262a83fcb0a7b88c505d4
loyaltypollution <65063925+loyaltypollution@users.noreply.github.com>
2025-08-06 09:13:39 +0000

    add testing for phitest

--- playground/input/phitest.java
+++ playground/input/phitest.java
@@ -2,7 +2,11 @@ public class phitest {
     public static boolean cond = true;
     public static void main(String[] args) {
         int x = 0;
-        if (cond) { x = 1; } else { x = 2; }
+        if (cond) {
+            x = 1;
+        } else {
+            x = 2;
+        }
         int y = x + 3;
         System.out.println(y);
     }
